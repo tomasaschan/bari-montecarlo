4895f25d1278b1fbf1b508aef30b13ed8bc83dd4
Tomas Lycken <tlycken@kth.se>
2013-04-09 14:57:46 +0200

    I scratch your back...

--- scratch/readcs/interpolate_cs_data.f
+++ scratch/readcs/interpolate_cs_data.f
@@ -1,15 +1,18 @@
-      subroutine interpolate_2d_data(nrd,data,nri,interp,x0,x1)
+      subroutine interpolate_2d_data(nrd,dta,nri,interp,x0,x1)
         implicit none
 
         ! row counts and pointers for both series
         integer nrd, nri, id, ii
         ! matrices holding x (first column) and y (second column)
-        real*8, intent(in) :: data(nrd,2)
+        real*8, intent(in) :: dta(nrd,2)
         real*8 :: interp(nri,2)
         ! boundaries for the desired interval and interpolation step size
         real*8 x0, x1, dx
         ! some intermediate quantities, used in calculations
-        real*8 k, xi
+        real*8 k, xi, xd(nri), yd(nri)
+
+        xd = dta(:,1)
+        yd = dta(:,2)
 
         ! set both pointers to start of ranges
         id = 1
@@ -17,70 +20,45 @@
         ! calculate step size
         dx = (x1-x0)/float(nri-1)
 
-        print *, "# dx: ", dx
+        !print *, "# dx: ", dx
 
         ! set first point of interpolation interval
         interp(1,1) = x0
         
-        if (x0 .lt. data(1,1)) then
-          ! desired x0 is outside of range.
+        if (x0 .gt. xd(2)) then
+          ! desired x0 is after second dta point.
+          ! step the dta range so x0 is in the current interval
+          print *, "# stepping dta range"
 
-          ! extrapolate so we get an (x0,y0)-pair
-          print *, "# TODO: extrapolate to x0"
-        elseif (x0 .eq. data(1,1)) then
-          ! start value is the same - we can use it directly
-          interp(1,2) = data(1,2)
-          ii = ii+1
-          id = id+1
-        else
-          ! desired x0 is inside first data point.
-
-          ! step the data range up to just before x0
-          do while(data(id,1) .lt. x0)
+          do while(xd(id) .lt. x0)
             id = id+1
           enddo
           ! the loop above steps one step too far
           id = id - 1
         endif
 
-        ! the x value for the first data point we'll interpolate to
         xi = x0 + (ii-1)*dx
 
-!        print *, "# ", ii, x0, data(id-1,1), xi, data(id,1), x1
+        do while(xd(id) .lt. x1 .and. id .lt. nrd)
+          k = (yd(id+1)-yd(id))/(xd(id+1)-xd(id))
 
-        do while(data(id,1) .lt. x1 .and. id .lt. nrd)
-          !print *, "# ", ii, id
-
-          k = (data(id+1,2)-data(id,2))/(data(id+1,1)-data(id,1))
-
-          do while (xi .lt. data(id, 1))
+          do while (xi .lt. xd(id+1) .and. ii .le. nri)
             interp(ii, 1) = xi
-            interp(ii, 2) = k*(xi-data(id,1)) + data(id,2)
+            interp(ii, 2) = yd(id) + k*(xi-xd(id))
             ii = ii+1
-            xi = x0 + (ii-1)*dx
+            xi = xi+dx
           enddo
 
           id = id+1
-!          print *, "# ", ii, x0, data(id-1,1), xi, data(id,1), x1
+!          print *, "# ", ii, x0, dta(id-1,1), xi, dta(id,1), x1
         enddo
 
         ! if there is some part of the interval left, extrapolate to x1
-        do while (xi .lt. x1)
+        do while (xi .le. x1 .and. ii .le. nri)
           ! use the same k as in the last interval
-          interp(ii,1) = xi
-          interp(ii,2) = k*(xi-data(id,1))+data(id,2)
+          interp(ii, 1) = xi
+          interp(ii, 2) = yd(id) + k*(xi-xd(id))
           xi = x0 + ii*dx
           ii = ii+1
         enddo
-
-!        print *, "# ", ii, x0, data(id-1,1), xi, data(id,1), x1
-
-!        print *, "# dx: ", dx, ", len: ", dx*nri
-
-        if (x1 .gt. data(size(data,1),1)) then 
-          print *, "# TODO: extrapolate to x1"
-        endif
-
-
-
       end
--- scratch/readcs/tester.f
+++ scratch/readcs/tester.f
@@ -6,7 +6,7 @@
         real*8, allocatable :: cs(:,:)
 
         integer :: Nrows_i, Ncols_interp
-        parameter(Nrows_i=1e5, Ncols_interp=Ncols)
+        parameter(Nrows_i=1e3, Ncols_interp=Ncols)
         real*8 :: interp(Nrows_i, Ncols_interp)
         real*8 x0, x1
         character*30 fname
@@ -18,13 +18,14 @@
         allocate(cs(Nrows,Ncols))
 
         print *, "# Reading from file ", fname
+        print *, "# x0: ", x0, ", x1: ", x1
 
         call read_cross_section_data(fname, Nrows, Ncols, cs)
 
         call interpolate_2d_data(Nrows,cs,Nrows_i,interp,x0,x1)
 
-        do row = 1, Nrows_i-1
-          print *, row, (interp(row,col),col=1,Ncols)
+        do row = 1, Nrows_i
+          print *, row, interp(row,1), interp(row,2)
         enddo
 
       end
